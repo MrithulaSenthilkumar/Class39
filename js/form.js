f59f04e28bec32e7960c36e0a3b5092ab05955a6
MrithulaSenthilkumar <74912521+MrithulaSenthilkumar@users.noreply.github.com>
2021-02-25 08:26:27 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,40 @@
+class Form{
+    constructor(){
+      this.title= createElement("h2");
+      this.input= createInput("Enter Your Name");
+      this.button= createButton("play");
+      this.greeting= createElement("h3");
+    }
+
+
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+      }
+
+display(){
+  
+   this.title.html ("Car Racing Game");
+   this.title.position(displayWidth/2+80,0);
+
+   this.input.position(displayWidth/2+80,displayHeight/2);
+   this.button.position(displayWidth/2+140,displayHeight/2+70);
+
+   this.button.mousePressed(()=>{
+    this.input.hide();
+    this.button.hide();
+
+    playerCount+=1;
+    player.name= this.input.value();
+    player.index=playerCount;
+ 
+    
+    player.updateName(player.name);
+    player.updateCount(playerCount);
+   
+    this.greeting.html("Hello welcome " + player.name);
+    this.greeting.position(displayWidth/2,displayHeight/2);
+})
+    }
+}
